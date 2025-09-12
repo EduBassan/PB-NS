5fb3c4bad158260fbf05faecce5e5cec34001abf
EduBassan <edubassan7@gmail.com>
2025-09-11 22:01:03 -0300

    hero-landing

--- src/layouts/LandingLayout.jsx
+++ src/layouts/LandingLayout.jsx
@@ -1,4 +1,5 @@
 import { Outlet } from "react-router-dom";
+import Hero from "../components/hero";
 
 export default function LandingLayout () {
     return(
@@ -6,8 +7,7 @@ export default function LandingLayout () {
             <div className="flex justify-center items-center text-center bg-[#3C1A6E] p-1 ">
                 <h1 className="flex flex-row gap-2 text-white"><strong>CHUTA</strong> ou <strong>PASSA A BOLA</strong></h1>
             </div>
-            <h1>Hello World</h1>
-            <Outlet/>
+            <Hero/>
         </>
     )
 }
