1dc3e498e36c420096d998b4e5402cc19832eb2d
MrJoaoAbe <joaovictorsouzaabe@gmail.com>
2025-09-13 15:58:17 -0300

    add-responsividade

--- src/components/CardJogos.jsx
+++ src/components/CardJogos.jsx
@@ -8,54 +8,152 @@ import Palmeiras from "../assets/logo-palmeiras-4096.png"
 export default function CardJogos(){
 
     return  (
-         <div className="flex justify-center items-center pb-20">
-            <div className="bg-[#3C1A6E] text-white rounded-xl shadow-lg p-6 w-150 h-70 mx-auto">
-                <div className="flex justify-between items-center text-sm font-bold mb-2">
-                    <span className="border-b border-white text-2xl">Time1</span>
-                    <span className="border-b border-white text-2xl">Time2</span>
+        <div>
+             <div className="lg:flex flex-row hidden justify-center items-center pb-20">
+                <div className="bg-[#3C1A6E] text-white rounded-xl shadow-lg p-6 w-150 h-70 mx-auto mr-5 ml-5">
+                    <div className="flex justify-between items-center text-sm font-bold mb-2">
+                        <span className="border-b border-white text-2xl">Time1</span>
+                        <span className="border-b border-white text-2xl">Time2</span>
+                    </div>
+                    <div className="flex justify-between items-center">
+                        <img src={SaoPaulo} alt="Time1" className="w-20" />
+                        <span className="text-5xl font-bold">VS</span>
+                        <img src={BotaFogo} alt="Time2" className="w-20" />
+                    </div>
+                    <div className="flex justify-center items-center mt-4">
+                        <span className="text-6xl font-extrabold">0 x 0</span>
+                    </div>
                 </div>
-                <div className="flex justify-between items-center">
-                    <img src={SaoPaulo} alt="Time1" className="w-20" />
-                    <span className="text-5xl font-bold">VS</span>
-                    <img src={BotaFogo} alt="Time2" className="w-20" />
-                </div>
-                <div className="flex justify-center items-center mt-4">
-                    <span className="text-6xl font-extrabold">0 x 0</span>
-                </div>
-            </div>
 
-            <div className="bg-[#3C1A6E] text-white rounded-xl shadow-lg p-6 w-150 h-70 mx-auto">
-                <div className="flex justify-between items-center text-sm font-bold mb-2">
-                    <span className="border-b border-white text-2xl">Time1</span>
-                    <span className="border-b border-white text-2xl">Time2</span>
+                <div className="bg-[#3C1A6E] text-white rounded-xl shadow-lg p-6 w-150 h-70 mx-auto mr-5 ml-5">
+                    <div className="flex justify-between items-center text-sm font-bold mb-2">
+                        <span className="border-b border-white text-2xl">Time1</span>
+                        <span className="border-b border-white text-2xl">Time2</span>
+                    </div>
+                    <div className="flex justify-between items-center">
+                        <img src={SaoPaulo} alt="Time1" className="w-20" />
+                        <span className="text-5xl font-bold">VS</span>
+                        <img src={BotaFogo} alt="Time2" className="w-20" />
+                    </div>
+                    <div className="flex justify-center items-center mt-4">
+                        <span className="text-6xl font-extrabold">0 x 0</span>
+                    </div>
                 </div>
-                <div className="flex justify-between items-center">
-                    <img src={SaoPaulo} alt="Time1" className="w-20" />
-                    <span className="text-5xl font-bold">VS</span>
-                    <img src={BotaFogo} alt="Time2" className="w-20" />
-                </div>
-                <div className="flex justify-center items-center mt-4">
-                    <span className="text-6xl font-extrabold">0 x 0</span>
-                </div>
-            </div>
 
-            <div className="bg-[#3C1A6E] text-white rounded-xl shadow-lg p-6 w-150 h-70 mx-auto">
-                <div className="flex justify-between items-center text-sm font-bold mb-2">
-                    <span className="border-b border-white text-2xl">Time1</span>
-                    <span className="border-b border-white text-2xl">Time2</span>
-                </div>
-                <div className="flex justify-between items-center">
-                    <img src={SaoPaulo} alt="Time1" className="w-20" />
-                    <span className="text-5xl font-bold">VS</span>
-                    <img src={BotaFogo} alt="Time2" className="w-20" />
-                </div>
-                <div className="flex justify-center items-center mt-4">
-                    <span className="text-6xl font-extrabold">0 x 0</span>
+                <div className="bg-[#3C1A6E] text-white rounded-xl shadow-lg p-6 w-150 h-70 mx-auto mr-5 ml-5">
+                    <div className="flex justify-between items-center text-sm font-bold mb-2">
+                        <span className="border-b border-white text-2xl">Time1</span>
+                        <span className="border-b border-white text-2xl">Time2</span>
+                    </div>
+                    <div className="flex justify-between items-center">
+                        <img src={SaoPaulo} alt="Time1" className="w-20" />
+                        <span className="text-5xl font-bold">VS</span>
+                        <img src={BotaFogo} alt="Time2" className="w-20" />
+                    </div>
+                    <div className="flex justify-center items-center mt-4">
+                        <span className="text-6xl font-extrabold">0 x 0</span>
+                    </div>
                 </div>
             </div>
 
             
+        {/* Tamanho SM */}
+        <div className="flex flex-col sm:hidden justify-center items-center pb-10">
+                <div className="bg-[#3C1A6E] text-white rounded-xl shadow-lg p-6 w-100 h-70 mx-auto mb-10">
+                    <div className="flex justify-between items-center text-sm font-bold mb-2">
+                        <span className="border-b border-white text-2xl">Time1</span>
+                        <span className="border-b border-white text-2xl">Time2</span>
+                    </div>
+                    <div className="flex justify-between items-center">
+                        <img src={SaoPaulo} alt="Time1" className="w-20" />
+                        <span className="text-5xl font-bold">VS</span>
+                        <img src={BotaFogo} alt="Time2" className="w-20" />
+                    </div>
+                    <div className="flex justify-center items-center mt-4">
+                        <span className="text-6xl font-extrabold">0 x 0</span>
+                    </div>
+                </div>
 
+                <div className="bg-[#3C1A6E] text-white rounded-xl shadow-lg p-6 w-100 h-70 mx-auto mb-10">
+                    <div className="flex justify-between items-center text-sm font-bold mb-2">
+                        <span className="border-b border-white text-2xl">Time1</span>
+                        <span className="border-b border-white text-2xl">Time2</span>
+                    </div>
+                    <div className="flex justify-between items-center">
+                        <img src={SaoPaulo} alt="Time1" className="w-20" />
+                        <span className="text-5xl font-bold">VS</span>
+                        <img src={BotaFogo} alt="Time2" className="w-20" />
+                    </div>
+                    <div className="flex justify-center items-center mt-4">
+                        <span className="text-6xl font-extrabold">0 x 0</span>
+                    </div>
+                </div>
+
+                <div className="bg-[#3C1A6E] text-white rounded-xl shadow-lg p-6 w-100 h-70 mx-auto mb-10">
+                    <div className="flex justify-between items-center text-sm font-bold mb-2">
+                        <span className="border-b border-white text-2xl">Time1</span>
+                        <span className="border-b border-white text-2xl">Time2</span>
+                    </div>
+                    <div className="flex justify-between items-center">
+                        <img src={SaoPaulo} alt="Time1" className="w-20" />
+                        <span className="text-5xl font-bold">VS</span>
+                        <img src={BotaFogo} alt="Time2" className="w-20" />
+                    </div>
+                    <div className="flex justify-center items-center mt-4">
+                        <span className="text-6xl font-extrabold">0 x 0</span>
+                    </div>
+                </div>
+            </div>
+
+        {/* Tamanho MD */}
+        <div className="hidden md:flex lg:hidden justify-center items-center pb-10">
+                <div className="bg-[#3C1A6E] text-white rounded-xl shadow-lg p-6 w-70 h-70 mx-auto mb-10 mr-5 ml-5">
+                    <div className="flex justify-between items-center text-sm font-bold mb-2">
+                        <span className="border-b border-white text-2xl">Time1</span>
+                        <span className="border-b border-white text-2xl">Time2</span>
+                    </div>
+                    <div className="flex justify-between items-center">
+                        <img src={SaoPaulo} alt="Time1" className="w-15" />
+                        <span className="text-3xl font-bold">VS</span>
+                        <img src={BotaFogo} alt="Time2" className="w-15" />
+                    </div>
+                    <div className="flex justify-center items-center mt-4">
+                        <span className="text-6xl font-extrabold">0 x 0</span>
+                    </div>
+                </div>
+
+                <div className="bg-[#3C1A6E] text-white rounded-xl shadow-lg p-6 w-70 h-70 mx-auto mb-10 mr-5 ml-5">
+                    <div className="flex justify-between items-center text-sm font-bold mb-2">
+                        <span className="border-b border-white text-2xl">Time1</span>
+                        <span className="border-b border-white text-2xl">Time2</span>
+                    </div>
+                    <div className="flex justify-between items-center">
+                        <img src={SaoPaulo} alt="Time1" className="w-15" />
+                        <span className="text-3xl font-bold">VS</span>
+                        <img src={BotaFogo} alt="Time2" className="w-15" />
+                    </div>
+                    <div className="flex justify-center items-center mt-4">
+                        <span className="text-6xl font-extrabold">0 x 0</span>
+                    </div>
+                </div>
+
+                <div className="bg-[#3C1A6E] text-white rounded-xl shadow-lg p-6 w-70 h-70 mx-auto mb-10 mr-5 ml-5">
+                    <div className="flex justify-between items-center text-sm font-bold mb-2">
+                        <span className="border-b border-white text-2xl">Time1</span>
+                        <span className="border-b border-white text-2xl">Time2</span>
+                    </div>
+                    <div className="flex justify-between items-center">
+                        <img src={SaoPaulo} alt="Time1" className="w-15" />
+                        <span className="text-3xl font-bold">VS</span>
+                        <img src={BotaFogo} alt="Time2" className="w-15" />
+                    </div>
+                    <div className="flex justify-center items-center mt-4">
+                        <span className="text-6xl font-extrabold">0 x 0</span>
+                    </div>
+                </div>
+            </div>
+
+    
     </div>
     );
 }
--- src/components/VideoFutebol.jsx
+++ src/components/VideoFutebol.jsx
@@ -1,16 +1,30 @@
 // src/components/VideoFutebol.jsx
 export default function VideoFutebol() {
   return (
-    <div className="flex justify-center items-center rounded-">
-      <iframe
-        width="800"
-        height="450"
-        src="https://www.youtube.com/embed/gqrbCI-T3B8"
-        title="Vídeo de Futebol"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      ></iframe>
+    <div>
+      <div className="flex lg:hidden justify-center items-center rounded-">
+        <iframe
+          width="470"
+          height="270"
+          src="https://www.youtube.com/embed/gqrbCI-T3B8"
+          title="Vídeo de Futebol"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      </div>
+
+      <div className="hidden lg:flex justify-center items-center rounded-">
+        <iframe
+          width="800"
+          height="450"
+          src="https://www.youtube.com/embed/gqrbCI-T3B8"
+          title="Vídeo de Futebol"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      </div>
     </div>
   );
 }
