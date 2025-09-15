0d4490aab324aaf1aedbc8ea74a44787afcd303f
Vitor Fernandes dos Santos <vitorfernandes11122006@gmail.com>
2025-09-15 17:30:39 -0300

    mudança no style dos cards

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -17,29 +17,29 @@ import "swiper/css/pagination";
 
 const atletas = [
     {
-        img:destaque1,
+        img: destaque1,
         nome: "Alê Xavier ",
         jogos: "13 Jogos ",
         gols: "9 Gols",
-        assist: "1 Assistência",
+        assist: "1 Assist.",
         pos: "Atacante",
         time: "São Paulo"
     },
     {
-        img:destaque2,
+        img: destaque2,
         nome: "Luana Maluf",
         jogos: "17 Jogos ",
         gols: "3 Gols ",
-        assist: "10 Assistências",
+        assist: "10 Assist.",
         pos: "Meio-Campo",
         time: "Palmeiras"
     },
     {
-        img:destaque3,
+        img: destaque3,
         nome: "Marcela Dantas",
         jogos: "15 Jogos ",
         gols: "4 Gols ",
-        assist: "8 Assistências",
+        assist: "8 Assist.",
         pos: "Atacante",
         time: "Palmeiras"
     }
@@ -47,15 +47,15 @@ const atletas = [
 
 export default function Hero() {
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 h-520">
-            <div className="md:hidden justify-center h-90 flex ">
+        <div className="grid grid-cols-1 md:col-span-1 lg:grid-cols-2 h-520">
+            <div className="md:flex lg:hidden justify-center md:h-120 h-90 flex bg-[#3C1A6E]">
                 <div >
                     <img src={atletasDisputa} alt="jogadoras disputando a bola" className="w-full" />
                 </div>
             </div>
             <div className="flex justify-center items-center flex-wrap h-120">
 
-                <div className="flex item-left flex-col pl-10 md:pl-40">
+                <div className="flex item-left flex-col pl-10 lg:pl-40">
                     <div className="block mb-2">
                         <img src={Logo} alt="logo-passa-a-bola" className="w-20" />
                     </div>
@@ -64,30 +64,30 @@ export default function Hero() {
 
                     <p className="block text-[#EE4D9A] font-bold text-[50px] leading-none mb-3 -translate-x-[3.5px]">FEITO POR ELAS</p>
 
-                    <h3 className="font-bold mb-3">Passa a Bola conecta meninas, times e comunidades para criar campeonatos acessíveis, divertidos e profissionais.</h3>
+                    <h3 className="font-bold mb-3 w-70 lg:w-110">Passa a Bola conecta meninas, times e comunidades para criar campeonatos acessíveis, divertidos e profissionais.</h3>
                     <Link to="/cadastrar" className="font-bold text-center bg-[#EE4D9A] border-2 p-3  w-50
                     hover:translate-x-7 text-white transition-all duration-400">Inscreva seu time</Link>
                     <Link to="/copa" className="text-[12px] font-bold text-center bg-[#3C1A6E] border-2 p-3  w-50
                      text-white transition-all hover:translate-x-7 duration-400">Ver programação da COPA PB </Link>
                 </div>
             </div>
-            <div className="md:flex justify-center pt-5 h-120 hidden ">
+            <div className="lg:flex justify-center pt-5 h-120 hidden ">
                 <div >
                     <img src={atletasDisputa} alt="jogadoras disputando a bola" className="w-130" />
                 </div>
             </div>
-            <div className="flex justify-center items-center flex-wrap h-100 bg-[#3C1A6E] col-span-1 md:col-span-2">
+            <div className="flex justify-center items-center flex-wrap h-100 bg-[#3C1A6E] col-span-1 lg:col-span-2">
                 <div>
-                    <h2 className="inline-block text-white font-bold text-[40px] leading-none mb-3 w-full text-center">Por que o Passa a Bola?</h2>
-                    <h2 className="inline-block text-white font-bold text-[18px] leading-none w-full text-center">O campeonato amador que apresenta uma solução para cada dificuldade do Futebol Feminino</h2>
+                    <h2 className="inline-block text-white font-bold text-[25px] lg:text-[40px] leading-none mb-3 w-full text-center">Por que o Passa a Bola?</h2>
+                    <h2 className="inline-block text-white font-bold text-[15px] lg:text-[18px] leading-none w-full text-center">O campeonato amador que apresenta uma solução para cada dificuldade do Futebol Feminino</h2>
                 </div>
                 <div className="grid grid-cols-10 bg-white p-5 justify-center items-center">
                     <div className="h-full col-span-3">
-                        <img src={equipe} alt="equipe da primeira edição da Copa Passa Bola" className="w-70" />
+                        <img src={equipe} alt="equipe da primeira edição da Copa Passa Bola" className="hidden lg:block w-70" />
                     </div>
                     <div className="col-span-7 flex justify-center flex-col items-center">
                         <p className="">Falta de oportunidades...</p>
-                        <h2 className="block text-[#EE4D9A] font-bold text-[40px] leading-none">→ Plataforma de inscrição simples.</h2>
+                        <h2 className="block text-[#EE4D9A] font-bold text-[20px] lg:text-[40px] leading-none">→ Plataforma de inscrição simples.</h2>
                     </div>
                 </div>
             </div>
@@ -120,65 +120,103 @@ export default function Hero() {
             </div> */}
 
 
-            <div className="flex justify-center items-center flex-wrap h-200 bg-white col-span-1 md:col-span-2">
+            <div className="flex justify-center items-center flex-wrap h-150 bg-white col-span-1 lg:col-span-2">
                 <p className="block text-[#EE4D9A] font-bold text-[50px] leading-none w-full text-center">Atletas em Destaque</p>
-                <div>
-                <Swiper
-                    modules={[Navigation, Pagination]}
-                    effect="coverflow"
-                    grabCursor={true}
-                    centeredSlides={true}
-                    slidesPerView={3}
-                    coverflowEffect={{
-                        rotate: 0,
-                        stretch: 0,
-                        depth: 100,
-                        modifier: 1,
-                        slideShadows: false,
-                    }}
-                    navigation
-                >
-                    {atletas.map((card, index) => (
-                        <SwiperSlide key={index} className="flex flex-col items-center justify-center p-5 shadow-lg bg-[#EE4D9A]">
-                            <div className="flex justify-center items-center bg-[#3C1A6E]">
-                            <img src={card.img} alt={card.nome} className="w-70" />
-                            </div>
-                            <p className="text-white font-bold text-[30px] text-center">{card.nome}</p>
-                            <h2 className="text-white font-bold text-xl text-center">{card.jogos}</h2>
-                            <h2 className="text-white font-bold text-xl text-center">{card.gols}</h2>
-                            <h2 className="text-white font-bold text-xl text-center">{card.assist}</h2>
-                            <h2 className="text-white font-bold text-xl text-center">{card.time}</h2>
-                            <h2 className="text-white font-bold text-xl text-center">{card.pos}</h2>
-                        </SwiperSlide>
-                    ))}
-                </Swiper>
-                </div>
-                <style>
-                    {`
-                        .swiper-slide {
-                            transform: scale(0.9) !important;
-                            opacity: 0.4 !important;
-                            transition: transform 0.5s ease, opacity 0.5s ease;
-                        }
+                <div className="w-full flex justify-center overflow-hidden">
+                    <div className="w-250 overflow-hidden">
+                        <Swiper
+                            modules={[Navigation, Pagination]}
+                            effect="coverflow"
+                            grabCursor={true}
+                            spaceBetween={1}
+                            navigation
+                            breakpoints={{
+                                0: {
+                                    slidesPerView: 1,
+                                    centeredSlides: false,
+                                    coverflowEffect: { rotate: 0, stretch: 0, depth: 50, modifier: 1, slideShadows: false },
+                                },
+                                640: {
+                                    slidesPerView: 1,
+                                    centeredSlides: false,
+                                    coverflowEffect: { rotate: 0, stretch: 0, depth: 60, modifier: 1, slideShadows: false },
+                                },
+                                768: { slidesPerView: 2, centeredSlides: false, coverflowEffect: { depth: 0, modifier: 0, slideShadows: false } },
+                                1024: { slidesPerView: 3, centeredSlides: false, coverflowEffect: { depth: 0, modifier: 0, slideShadows: false } },
+                            }}
+                        >
+                            {atletas.map((card, index) => (
+                                <SwiperSlide
+                                    key={index}
+                                    className="flex flex-row flex-nowrap items-center justify-center"
+                                >
+                                    <div className="bg-gradient-to-br from-[#713bc2] via-[#3c1970] to-[#5927a3] p-4 w-80">
+                                        <p className="w-full text-white font-bold text-[30px] text-center leading-none">{card.nome}</p>
+                                        <div className="flex justify-center">
+                                            <div className="flex flex-col justify-start bg-[#EE4D9A]- overflow-hidden h-70 w-70">
 
-                        .swiper-slide-active {
-                            transform: scale(1) !important;
-                            opacity: 1 !important;
-                        }
-                         .swiper-button-next, .swiper-button-prev {
-                            color: #EE4D9A;
-                            background-color: rgba(255,255,255,0.2);
-                            width: 40px;
-                            height: 40px;
-                        }
+                                                <h2 className=" relative z-15 w-full text-center text-white font-bold text-xl">{card.pos}</h2>
+                                                <img
+                                                    src={card.img}
+                                                    alt={card.nome}
+                                                    className="w-full max-w-[250px] md:max-w-[280px] lg:max-w-[300px]"
+                                                />
 
-                        .swiper-button-next:hover, .swiper-button-prev:hover {
-                            color: #3C1A6E;
-                        } 
+                                            </div>
+                                        </div>
+                                        <div className="flex flex-row flex-nowrap items-center justify-center gap-20 ">
+                                            <div className="flex flex-col justify-left h-full">
+                                                <h2 className="w-full text-white font-bold text-xl text-center">{card.jogos}</h2>
+                                                <h2 className="text-white font-bold text-xl text-center">{card.time}</h2>
+                                            </div >
+
+                                            <div className="h-full">
+                                                <h2 className="text-white font-bold text-xl text-center">{card.gols}</h2>
+                                                <h2 className="text-white font-bold text-xl text-center">{card.assist}</h2>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </SwiperSlide>
+                            ))}
+                        </Swiper>
+
+                    </div>
+
+
+
+                    <style>
+                        {`
+                            .swiper-slide {
+                                transform: scale(0.9) !important;
+                                opacity: 0.4 !important;
+                                transition: transform 0.5s ease, opacity 0.5s ease;
+                            }
+
+                            .swiper-slide-active {
+                                transform: scale(1) !important;
+                                opacity: 1 !important;
+                            }
+
+                            .swiper-button-next,
+                            .swiper-button-prev {
+                                color: #EE4D9A;
+                                background-color: rgba(255,255,255,0.2);
+                                width: 40px;
+                                height: 40px;
+                                border-radius: 9999px;
+                            }
+
+                            .swiper-button-next:hover,
+                            .swiper-button-prev:hover {
+                                color: #3C1A6E;
+                            }
                         `}
-                </style>
+                    </style>
                 </div>
+
+
             </div>
+        </div>
 
 
 
