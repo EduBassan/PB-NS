00647f1066b88a830f360606592ae05aa3ff13ad
vitor-1fernandes <vitorfernandes11122006@gmail.com>
2025-11-28 22:44:32 -0300

    ajuste para exibir jogos importados do local storage

--- src/copa/JogosCopa.jsx
+++ src/copa/JogosCopa.jsx
@@ -1,48 +1,15 @@
-import React from "react";
-
-const jogos = [
-  {
-    id: 1,
-    data: "Nov 19",
-    timeCasa: "SPFC",
-    logoCasa: "https://logodetimes.com/times/sao-paulo/logo-sao-paulo-4096.png",
-    golsCasa: 2,
-    timeFora: "SFC",
-    logoFora: "https://logodetimes.com/times/santos/logo-santos-1536.png",
-    golsFora: 0,
-    estadio: "Morumbi",
-    hora: "21:30h",
-    hashtag: "#ChoqueDeGigantes",
-  },
-  {
-    id: 2,
-    data: "Nov 20",
-    timeCasa: "Corinthians",
-    logoCasa: "https://logodetimes.com/times/corinthians/logo-corinthians-4096.png",
-    golsCasa: 3,
-    timeFora: "Palmeiras",
-    logoFora: "https://imagepng.org/wp-content/uploads/2018/03/escudo-palmeiras.png",
-    golsFora: 2,
-    estadio: "Neo Química Arena",
-    hora: "20:00h",
-    hashtag: "#DerbyPaulista",
-  },
-  {
-    id: 3,
-    data: "Nov 21",
-    timeCasa: "Flamengo",
-    logoCasa: "https://th.bing.com/th/id/R.c7f858adcf5c6f5cd97f4b2a263cd5b1?rik=eksXhOCTAYsIrg&pid=ImgRaw&r=0",
-    golsCasa: 1,
-    timeFora: "Vasco",
-    logoFora: "https://tse4.mm.bing.net/th/id/OIP.rnihwMvzGnzLsELBY_PE3wHaHa?rs=1&pid=ImgDetMain&o=7&rm=3",
-    golsFora: 1,
-    estadio: "Maracanã",
-    hora: "18:00h",
-    hashtag: "#ClassicoCarioca",
-  },
-];
+import React, { useEffect, useState } from "react";
+import {faPencil} from "@fortawesome/free-solid-svg-icons";
 
 export default function JogosCopa() {
+  const [jogos, setTimes] = useState([])
+
+  useEffect(() => {
+  const raw = localStorage.getItem("jogos");
+  const dados = raw ? JSON.parse(raw) : [];
+  setTimes(dados)
+}, [jogos])
+
   return (
     <section className="bg-white text-black py-10 px-4">
       <div className="text-center mb-8">
@@ -54,7 +21,7 @@ export default function JogosCopa() {
       <div className="relative max-w-6xl mx-auto">
 
         <div className="flex gap-8 justify-center items-center flex-wrap">
-          {jogos.map((jogo) => (
+          {jogos.filter((a) => a.exibir == true).map((jogo) => (
             <div
               key={jogo.id}
               className="border border-pink-200 rounded-lg shadow-md w-full max-w-[320px] p-5 flex flex-col items-center"
@@ -93,7 +60,7 @@ export default function JogosCopa() {
                 {jogo.estadio} – {jogo.hora}
               </p>
 
-              <button className="mt-4 bg-[#EE4D9A] border-2 border-[#EE4D9A] text-white w-[95%] font-semibold text-sm py-2 px-6 hover:bg-white hover:text-[#EE4D9A] transition-all hover:duration-500">
+              <button className="mt-4 bg-[#EE4D9A] border-2 border-[#EE4D9A] text-white w-[95%] font-semibold text-sm py-2 px-6 hover:bg-white hover:text-[#EE4D9A] transition-all hover:duration-500" onClick={() => alert("Você votou em")}>
                 VOTAR
               </button>
             </div>
